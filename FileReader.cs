75f039b414302fdcfc7e0293bbf38a8f13467ada
Prakarsh Srivastava <Prakarsh_Srivastava@epam.com>
2022-04-25 10:30:00 +0530

    Add project files.

---
+++ FileReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    public class FileReader:IDisposable
+    {
+        StreamReader sr = null;
+        private bool _disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void Reader()
+        {
+            try
+            {
+                sr = new StreamReader("C:\\Users\\Prakarsh_Srivastava\\OneDrive - EPAM\\MemoryManagement\\Sample.txt");
+                
+                string line;
+                    
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Dispose();
+                Console.WriteLine("Disposed");
+            }
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            if (disposing)
+            {
+                //objSafeHandle.Dispose();
+                Console.WriteLine("Clean up all managed objs and child also that implements ");
+            }
+
+
+            Console.WriteLine("Clean up all unmanaged..");
+            sr.Close();
+
+            _disposed = true;
+        }
+        ~FileReader()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader();
+            fileReader.Reader();
+            Console.ReadLine();
+        }
+    }
+    
+}
